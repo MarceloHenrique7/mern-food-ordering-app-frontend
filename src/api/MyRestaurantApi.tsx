5871fda5f9eb313a9908ea75e807ca40080411ff
MarceloHenrique7 <marcelohenriquex77@gmail.com>
2024-03-15 00:26:19 -0300

    corrigindo erro

--- src/api/MyRestaurantApi.tsx
+++ src/api/MyRestaurantApi.tsx
@@ -1,6 +1,5 @@
 import { Restaurant } from "@/types";
 import { useAuth0 } from "@auth0/auth0-react"
-import { uptime } from "process";
 import { useMutation, useQuery } from "react-query";
 import { toast } from "sonner";
 
